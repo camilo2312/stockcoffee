785742f85cb0576866240188a22d720b7b155dd4
camilo2312 <42982644+camilo2312@users.noreply.github.com>
2024-02-10 16:16:23 -0500

    Primer commit del proyecto

---
+++ src/main/java/co/edu/uniquindio/stockcoffee/StockCoffeeApplication.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.stockcoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockCoffeeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StockCoffeeApplication.class, args);
+    }
+}
